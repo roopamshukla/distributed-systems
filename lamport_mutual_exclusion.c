b8d4641ce5e98d8c2aabc57fb69a93bc58460963
Roopam Shukla <roopamscshukla@gmail.com>
2017-08-31 19:21:02 +0530

    request CS initial

--- lamport_mutual_exclusion.c
+++ lamport_mutual_exclusion.c
@@ -1,45 +1,93 @@
 #include<stdio.h>
 #include<stdlib.h>
-int columns = 0;
+struct que_ele {
+    int pname;
+    int tstamp;
+} que_ele;
+int columns = 0, q_size = 0, flag = 0, count = 0;
+struct que_ele *csq;
+int *latest_count, *event_counter, **count_matrix;
 void add_event(int p1, int p2, int *latest_count, int *event_counter,
                int **count_matrix, int processes)
 {
-    if(event_counter[p1-1]>=columns){
-    ++columns;
-    int i = 0, *temp;
-    for (i = 0; i < processes; ++i) {
-        temp = realloc(count_matrix[i], columns);
-        if (temp != count_matrix[i])
-            free(count_matrix[i]);
-        count_matrix[i] = temp;
+    if (event_counter[p1 - 1] >= columns
+            || event_counter[p2 - 1] >= columns) {
+        ++columns;
+        int i = 0, *temp;
+        for (i = 0; i < processes; ++i) {
+            temp = realloc(count_matrix[i], columns);
+            if (temp != count_matrix[i])
+                free(count_matrix[i]);
+            count_matrix[i] = temp;
+        }
     }
-}
     if (p2 == 0) {
         ++event_counter[p1 - 1];
         ++latest_count[p1 - 1];
         count_matrix[p1 - 1][event_counter[p1 - 1] - 1] =
             latest_count[p1 - 1];
     } else {
-        if (latest_count[p1 - 1] >= latest_count[p2 - 1]) {
+        if (flag == 1) {
+            if (latest_count[p1 - 1] >= latest_count[p2 - 1]) {
+                ++event_counter[p2 - 1];
+                count_matrix[p2 - 1][event_counter[p2 - 1] - 1] =
+                    latest_count[p1 - 1] + 1;
+                latest_count[p2 - 1] = latest_count[p1 - 1] + 1;
+            } else {
+                ++event_counter[p2 - 1];
+                ++latest_count[p2 - 1];
+                count_matrix[p2 - 1][event_counter[p2 - 1] - 1] =
+                    latest_count[p2 - 1];
+            }
+        } else {
             ++event_counter[p1 - 1];
             ++latest_count[p1 - 1];
             count_matrix[p1 - 1][event_counter[p1 - 1] - 1] =
                 latest_count[p1 - 1];
+        if (latest_count[p1 - 1] >= latest_count[p2 - 1]) {
+            ++event_counter[p2 - 1];
+            count_matrix[p2 - 1][event_counter[p2 - 1] - 1] =
+                latest_count[p1 - 1] + 1;
+            latest_count[p2 - 1] = latest_count[p1 - 1] + 1;
         } else {
-            ++event_counter[p1 - 1];
-            count_matrix[p1 - 1][event_counter[p1 - 1] - 1] =
-                latest_count[p2 - 1] + 1;
+            ++event_counter[p2 - 1];
+            ++latest_count[p2 - 1];
+            count_matrix[p2 - 1][event_counter[p2 - 1] - 1] =
+                latest_count[p2 - 1];
         }
+    }
+    }
+}
+
+void request_section(int e, int *latest_count, int *event_counter,
+                     int **count_matrix, int processes)
+{
+    int i = 0;
+    ++q_size;
+    csq = realloc(csq, q_size * sizeof(que_ele));
+    que_ele.pname = e - 1;
+    que_ele.tstamp = latest_count[e - 1];
+    csq[q_size - 1] = que_ele;
+    add_event(e, 0, latest_count, event_counter, count_matrix, processes);
+    for (i = 0; i < processes; i++) {
+        if (i != e - 1)
+            add_event(e,i+1, latest_count, event_counter, count_matrix,
+                      processes);
+    }
+    //reply
+    flag = 0;
+    for (i = 0; i < processes; i++) {
+            add_event(i+1,e, latest_count, event_counter, count_matrix,
+                      processes);
     }
 }
 
 int main()
 {
-    int n = 0, processes = 0, i = 0, j = 0, csq[20], cindex = 0, temp =
-            0, e = 0,p1=0,p2=0, flag = 0;
+    int n = 0, processes = 0, i = 0, j = 0, cindex = 0, temp =
+            0, e = 0, p1 = 0, p2 = 0;
     printf("Enter no of processes : ");
     scanf("%d", &processes);
-    int *latest_count, *event_counter, **count_matrix;
     latest_count = malloc(processes * sizeof(*latest_count));
     event_counter = malloc(processes * sizeof(*event_counter));
     count_matrix = malloc(processes * sizeof(*count_matrix));
@@ -64,17 +112,26 @@ int main()
         case 1:
             printf("Enter process number : ");
             scanf("%d", &e);
+            flag = 0;
             add_event(e, 0, latest_count, event_counter, count_matrix,
                       processes);
             break;
-        case 2:printf("Process From : ");
+        case 2:
+            printf("Process From : ");
             scanf("%d", &p1);
             printf("Process To : ");
             scanf("%d", &p2);
+            flag = 0;
             add_event(p1, p2, latest_count, event_counter, count_matrix,
                       processes);
             break;
         case 3:
+            printf("Enter process no : ");
+            scanf("%d", &e);
+            flag = 1;
+            count = 0;
+            request_section(e, latest_count, event_counter, count_matrix,
+                            processes);
             break;
         case 4:
             break;
